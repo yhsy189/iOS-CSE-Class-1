b7e8f8e606405cf43fd629b705724e5a2100d6a5
Alan Zeino <alan@Alans-MacBook-Air.local>
2011-03-29 15:09:09 +1100

    First commit.

---
+++ Classes/Class1AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  Class1AppDelegate.h
+//  Class1
+//
+//  Created by Alan Zeino on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Class1AppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
