1004320353056dc82f0a26a8a31da0863d5c516b
Yat Hei Sunny Yuen <yhsy189@chil10.orchestra.cse.unsw.edu.au>
2011-03-29 15:38:28 +1100

    Added Peron

---
+++ Person.h
@@ -0,0 +1,16 @@
+//
+//  Person.h
+//  Class1
+//
+//  Created by Yat Hei Sunny Yuen on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Person : NSObject {
+
+}
+
+@end
---
+++ Person.m
@@ -0,0 +1,14 @@
+//
+//  Person.m
+//  Class1
+//
+//  Created by Yat Hei Sunny Yuen on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Person.h"
+
+
+@implementation Person
+
+@end
